5e840cb1aa9b979b1695c3f852bc886950c1cb17
Akash Gupta <akashgupta989322@gmail.com>
2025-01-14 19:55:39 +0530

    restore Navbar component in RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <ReduxProvider>
-          {/* <Navbar /> */}
+          <Navbar />
           {children}
           <Footer />
         </ReduxProvider>
