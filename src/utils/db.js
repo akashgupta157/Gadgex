4f6beebd0f7009ca7e528d5f443d651c83d3b989
Akash Gupta <akashgupta989322@gmail.com>
2025-01-14 20:02:34 +0530

    remove unused files and components from the project

--- src/utils/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-export default async function dbConnect() {
-  try {
-    mongoose.connect(process.env.MONGODB_URI);
-    console.log("Database Connected");
-  } catch (error) {
-    console.log(error);
-  }
-}
