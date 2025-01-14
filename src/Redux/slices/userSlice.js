89193c3d222046d7db1aa31d0d86364aea313f8c
Akash Gupta <akashgupta989322@gmail.com>
2025-01-14 19:44:49 +0530

    refactor userSlice to remove local storage handling and set initial state

--- src/Redux/slices/userSlice.js
+++ src/Redux/slices/userSlice.js
@@ -1,25 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-const saveToLocalStorage = (key, value) => {
-  try {
-    localStorage.setItem(key, JSON.stringify(value));
-  } catch (error) {
-    console.error("Error saving to local storage", error);
-  }
-};
-
-const loadFromLocalStorage = (key) => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState ? JSON.parse(serializedState) : null;
-  } catch (error) {
-    console.error("Error loading from local storage", error);
-    return null;
-  }
-};
 const initialState = {
-  user: loadFromLocalStorage("user") || null,
-  isAuthenticated: !!loadFromLocalStorage("user"),
+  user: null,
+  isAuthenticated: false,
 };
 
 const userSlice = createSlice({
@@ -29,12 +11,10 @@ const userSlice = createSlice({
     setUser: (state, action) => {
       state.user = action.payload;
       state.isAuthenticated = true;
-      saveToLocalStorage("user", action.payload);
     },
     removeUser: (state) => {
       state.user = null;
       state.isAuthenticated = false;
-      localStorage.removeItem("user");
     },
   },
 });
