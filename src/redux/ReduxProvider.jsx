11550fcd22dd359a790832333296539da879aeef
Akash Gupta <akashgupta989322@gmail.com>
2025-01-21 10:10:47 +0530

    wrap ReduxProvider children with Suspense for improved loading state handling

--- src/redux/ReduxProvider.jsx
+++ src/redux/ReduxProvider.jsx
@@ -1,7 +1,12 @@
 "use client";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { Suspense } from "react";
 
 export default function ReduxProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    </Provider>
+  );
 }
